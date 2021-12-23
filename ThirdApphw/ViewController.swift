476480239236fa2bdd3b5770041bea59043c32c3
Мусаев94 <beksultan1923@gmail.com>
2021-12-24 01:51:23 +0600

    Initial Commit

---
+++ ThirdApphw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThirdApphw
+//
+//  Created by user on 24/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
